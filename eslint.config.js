6b27dea8ce3b0a230dcbbf3d6cce7e8aa178e3be
Wedney <wedney.santos@gmail.com>
2026-06-06 23:08:51 -0300

    Atualizando arquivo

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,24 @@
 import js from "@eslint/js";
 import globals from "globals";
-import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  pluginReact.configs.flat.recommended,
+  {
+    files: ["**/*.js"],
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
+
+  {
+    files: ["test/**/*.js"],
+    languageOptions: {
+      globals: {
+        ...globals.mocha,
+      },
+    },
+  },
 ]);
